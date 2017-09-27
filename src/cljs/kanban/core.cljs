72fdd6f646ff5b9f71f6384c2cc21a5a2ed824a9
vishalgautamm <vishal.g@live.ca>
2017-09-27 13:05:24 -0400

    App complete

--- src/cljs/kanban/core.cljs
+++ src/cljs/kanban/core.cljs
@@ -4,53 +4,98 @@
 (enable-console-print!)
 
 (def app-state
-  (r/atom
-   {:columns [{:title "Todos"
-               :cards [{:title "Learn about Reagent"}
-                       {:title "Learn React"}]}
-              {:title "Functional Programming"
-               :editing true
-               :cards [{:title "Elm is awesome"}
-                       {:title "Decoding sucks though"
-                        :editing true}]}
-              {:title "Clojure Programming Lang"
-               :cards [{:title "Clojure is amazing" :editing true}
-                       {:title "Formatting is a bitch though"
-                        :editing true}]}]}))
+  (r/atom {:columns [{:id (random-uuid)
+                      :title "Clojure"
+                      :cards [{:id (random-uuid)
+                               :title "Learn about Reagent"}
+                              {:id (random-uuid)
+                               :title "Learn React"}]}
+                     {:id (random-uuid)
+                      :title "Elm"
+                      :cards [{:id (random-uuid)
+                               :title "Elm is awesome"}
+                              {:id (random-uuid)
+                               :title "learn to elm"}]}]}))
+;
+(defn AutoFocusInput [props]
+  (r/create-class {:displayName "AutoFocusInput"
+                   :component-did-mount (fn [component]
+                                          (.focus (r/dom-node component)))
+                   :reagent-render (fn [props]
+                                     [:input props])}))
+;
+(defn- update-title [cursor title]
+  (swap! cursor assoc :title title))
+
+(defn- stop-editing [cursor]
+  (swap! cursor dissoc :editing))
+
+(defn- start-editing [cursor]
+  (swap! cursor assoc :editing true))
+;
+
 
 (defn Header []
   [:div.header
-   [:h2 "Crello"]])
+   [:h2 "Trello Clone"]])
 
-(defn Card [card]
-  (if (:editing card)
-    [:div.card.editing [:input {:type "text" :value (:title card)}]]
-    [:div.card (:title card)]))
 
-(defn NewCard []
+(defn Editable [el cursor]
+  (let [{:keys [editing title]} @cursor]
+    (if editing
+      [el {:className "editing"} [AutoFocusInput {:type "text"
+                                                  :value title
+                                                  :on-change #(update-title cursor (.. % -target -value))
+                                                  :on-blur #(stop-editing cursor)
+                                                  :on-key-press #(if (= (.-charCode %) 13)
+                                                                   (stop-editing cursor))}]]
+      [el {:on-click #(start-editing cursor)} title])))
+;
+(defn Card [cursor]
+    [Editable :div.card cursor])
+
+(defn add-new-card [col-cur]
+  (swap! col-cur update :cards conj {:id (random-uuid)
+                                     :title ""
+                                     :editing true}))
+
+(defn NewCard [col-cur]
   [:div.new-card
-   "+ a new card"])
+   {:on-click #(add-new-card col-cur)}
+   "+ add new card"])
 
-(defn Column [{:keys [title cards editing]}]
-  [:div.column
-   (if editing
-     [:input {:type "text" :value title}]
-     [:h3  title])
-   (for [c cards]
-     [Card c])
-   [NewCard]])
+(defn Column [col-cur]
+  (let [{:keys [title cards editing]} @col-cur]
+    [:div.column
+     ^{:key "title"} [Editable :h3 col-cur]
+     (map-indexed (fn [idx {id :id}]
+                    (let [card-cur (r/cursor col-cur [:cards idx])]
+                      ^{:key id} [Card card-cur]))
+                  cards)
+     ^{:key "new"} [NewCard col-cur]]))
 
-(defn NewColumn []
+(defn- add-new-column [board]
+  (swap! board update :columns conj {:id (random-uuid)
+                                     :title ""
+                                     :cards []
+                                     :editing true}))
+
+(defn NewColumn [board]
   [:div.new-column
-   "+ a new column"])
+   {:on-click #(add-new-column board)}
+   "+ add new column"])
 
-(defn Board [state]
-  [:div.wrapper
-   [Header]
-   [:div.board
-    (for [c (:columns @state)]
-      [Column c])
-    [NewColumn]]])
+(defn Board [board]
+  [:div.container
+    [Header]
+    [:div.board
+    (map-indexed (fn [idx {id :id}]
+                    (let [col-cur (r/cursor board [:columns idx])]
+                      ^{:key id} [Column col-cur]))
+                  (:columns @board))
+    ^{:key "new"} [NewColumn board]]])
+
+
 
 (defn render []
   (r/render [Board app-state] (js/document.getElementById "app")))
